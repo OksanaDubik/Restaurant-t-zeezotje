03b07a1447ad6cf21cb72d7c0c407a696e0ccb0f
Oksana <oksana170773@gmail.com>
2023-08-20 11:28:25 +0600

    доработана форма, добавлен фа

--- index.js
+++ index.js
@@ -32,10 +32,25 @@ document.addEventListener('DOMContentLoaded', function () {
 
     async function formSend(e) {
         e.preventDefault();
-        let error = formValidate(form);
+
+
+        let error = 0;
+        let formReq = document.querySelectorAll('._req')
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+
+            if (input.value === '') {
+                formAddError(input);
+                error++;
+            }
+        }
 
         let formData = new FormData(form);
+
         if (error === 0) {
+
             let response = await fetch('sendmail.php', {
                 method: 'POST',
                 body: formData
@@ -48,32 +63,19 @@ document.addEventListener('DOMContentLoaded', function () {
                 alert('error')
             }
         } else {
-            alert('Preencha os campos obrigatórios')
+            alert('Preencha os campos obrigatórios (Заполните обязательные поля.)')
         }
 
-    }
-
-    function formValidate(form) {
-        let error = 0;
-        let formReq = document.querySelectorAll('._req')
-
-        for (let index = 0; index < formReq.length; index++) {
-            const input = formReq[index];
-            formRemoveError(input);
-
-            if (input.value.trim() === '') {
-                formAddError(input);
-                error++;
-            }
-        }
     }
 
     function formAddError(input) {
         input.parentElement.classList.add('_error');
         input.classList.add('_error');
     }
+
     function formRemoveError(input) {
         input.parentElement.classList.remove('_error');
         input.classList.remove('_error');
     }
+
 });
